1d62fdef9c6ff9e297a87d3373605122ba68bf81
Nhựt Trần <tnhut803@gmail.com>
2022-11-08 20:38:39 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const CircleToPolygon = require("circle-to-polygon");
+const fs = require("fs");
+//coordinates tọa độ tâm, radius bán kính, numberOfEdges Số lượng điểm
+function createCircle(coordinates, radius, numberOfEdges) {
+  let polygon = CircleToPolygon(coordinates, radius, numberOfEdges);
+  polygon.coordinates[0].forEach((point, index) => {
+    if (coordinates[2]) {
+      polygon.coordinates[0][index] = [...point, coordinates[2]];
+    } else {
+      polygon.coordinates[0][index] = [...point, 0];
+    }
+    fs.writeFileSync("CoordinateCircle.json", JSON.stringify(polygon));
+  });
+}
+const coordinates = [12.4922323, 41.8901987, 5]; //[lon, lat]
+const radius = 40; // in meters
+const numberOfEdges = 500; //optional that defaults to 32
+createCircle(coordinates, radius, numberOfEdges); // kết quả cho ra trong file CoordinateCircle.json
