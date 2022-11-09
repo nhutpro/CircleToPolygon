87c704f12d23235170dac78489e7a9e77844821f
ZachHung <zachhung911@gmail.com>
2022-11-09 21:34:14 +0700

    added create pillars feature

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const CircleToPolygon = require("circle-to-polygon");
-const fs = require("fs");
+const CircleToPolygon = require('circle-to-polygon');
+const fs = require('fs');
+
 //coordinates tọa độ tâm, radius bán kính, numberOfEdges Số lượng điểm
 function createCircle(coordinates, radius, numberOfEdges) {
   let polygon = CircleToPolygon(coordinates, radius, numberOfEdges);
@@ -9,10 +10,33 @@ function createCircle(coordinates, radius, numberOfEdges) {
     } else {
       polygon.coordinates[0][index] = [...point, 0];
     }
-    fs.writeFileSync("CoordinateCircle.json", JSON.stringify(polygon));
   });
+  fs.writeFileSync('CoordinateCircle.json', JSON.stringify(polygon));
+  return polygon;
 }
-const coordinates = [12.4922309, 41.8902102, 5]; //[lon, lat]
-const radius = 40; // in meters
-const numberOfEdges = 500; //optional that defaults to 32
-createCircle(coordinates, radius, numberOfEdges); // kết quả cho ra trong file CoordinateCircle.json
+
+function createPillar(coordinates, radius, numberOfPillars) {
+  let circle = createCircle(coordinates, radius, numberOfPillars);
+  let arr = [];
+  let index = 0;
+  for (const pillarCenter of circle.coordinates[0]) {
+    let pillar = createCircle(pillarCenter, 1, 36);
+    let feature = {
+      type: 'Feature',
+      properties: { 'Pillar Number': index + 1, height: 40 },
+      geometry: {
+        type: 'Polygon',
+        coordinates: [pillar.coordinates[0]],
+      },
+      id: `floor${index}`,
+    };
+    arr.push(feature);
+    fs.writeFileSync('CoordinatePillar.json', JSON.stringify(arr));
+    ++index;
+  }
+}
+
+const coordinates = [12.4922309, 41.8902102, 0]; //[long, lat]
+const radius = 80; // in meters
+const numberOfEdges = 80; //optional that defaults to 32
+createPillar(coordinates, radius, numberOfEdges); // kết quả cho ra trong file CoordinateCircle.json và file CoordinatePillar.json
